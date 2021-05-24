ff99471712276dc8071c2aff3e6c43d8316f2bb9
Casper Sejrup <sejrup112@gmail.com>
2021-05-24 18:24:05 +0200

    Nodemailer added, to be tested.

--- Web/src/app/services/confirmation-service.service.ts
+++ Web/src/app/services/confirmation-service.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { environment } from './../../environments/environment';
 @Injectable({
   providedIn: 'root'
@@ -9,4 +11,24 @@ export class ConfirmationServiceService {
   private eventRegistrationUrl = environment.baseApiUrl+'eventRegistrations/';
   
   constructor(private cookieService: CookieService, private http: HttpClient) { }
+
+  private nodemailer = require('nodemailer');
+  public transporter = this.nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: '',
+      pass: ''
+    }
+  });
+
+  private mailOptions = {
+    from: '',
+    to: '',
+    subject: 'Race Registration Confirmation Email',
+    Text: 'You have been registered.'
+  }
+  public sendEmail(){
+
+  }
+
 }
