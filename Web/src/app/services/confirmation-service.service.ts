76990fbf890548033227b3fb4020f0ef0c52ae07
Casper Sejrup <sejrup112@gmail.com>
2021-05-24 18:33:49 +0200

    http get user email added.

--- Web/src/app/services/confirmation-service.service.ts
+++ Web/src/app/services/confirmation-service.service.ts
@@ -1,6 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../models/user';
 import { environment } from './../../environments/environment';
 @Injectable({
   providedIn: 'root'
@@ -8,10 +11,24 @@ import { environment } from './../../environments/environment';
 export class ConfirmationServiceService {
 
 
-  private eventRegistrationUrl = environment.baseApiUrl+'eventRegistrations/';
+  private userUrl = environment.baseApiUrl+'Users/';
   
   constructor(private cookieService: CookieService, private http: HttpClient) { }
-
+  /**
+   * 
+   * @returns userEmail
+   */
+  public getUserByEmail(): Observable<User[]> {
+    let user = JSON.parse(this.cookieService.get('user'));
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'x-access-token': user.token
+      })
+    }
+    return this.http.get<User[]>(this.userUrl+"myShips/fromUsername", httpOptions)
+      .pipe(map(user => { return user }));
+  }
   private nodemailer = require('nodemailer');
   public transporter = this.nodemailer.createTransport({
     service: 'gmail',
