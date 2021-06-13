b772e57f16ae0b68ebe4d05202542e2a2e30bbf0
Jens Christensen <paviln@outlook.dk>
2021-06-13 21:17:10 +0200

    Supertesting.

---
+++ Api/tests/user.test.ts
@@ -0,0 +1,20 @@
+import {describe, test, it, expect} from '@jest/globals';
+import * as app from "../src/app";
+import * as request from "supertest";
+import { IUser } from '../src/models/user';
+
+const user = {
+  firstname: "Jens",
+  lastname: "Christensen",
+  emailUsername: "paviln@outlook.dk",
+  password: "1234",
+  role: "user"
+};
+
+describe("POST /forgotPassword - reset password of user", () => {
+  it("Forgot Password API Request", async () => {
+    const result = await request(app).post("/api/user/forgotPassword");
+    expect(result.text).toEqual(user);
+    expect(result.statusCode).toEqual(200);
+  });
+});
