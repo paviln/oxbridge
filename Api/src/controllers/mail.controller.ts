d613aa8bd33c08c5c610eb30c0da6e051a60766d
Jens Christensen <paviln@outlook.dk>
2021-06-13 20:59:57 +0200

    Setup Api ready for testing.

--- Api/src/controllers/mail.controller.ts
+++
@@ -1,8 +0,0 @@
-import EventRegistration, {IEventRegistration} from '../models/eventRegistration';
-import Ship, {IShip} from '../models/ship';
-import User, {IUser} from '../models/user';
-import {Request, Response} from 'express';
-//Inits//
-
-
-
